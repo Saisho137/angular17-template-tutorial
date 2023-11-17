2f50b6b5830ff1299cb037c5ba5d24cfa8b479e6
Santiago <santib.2001@hotmail.com>
2023-11-17 12:56:42 -0500

    feat: learning about images optimizacion with Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgOptimizedImage  } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { LoginComponent } from './components/login/login.component';
@@ -7,13 +7,14 @@ import { LoginComponent } from './components/login/login.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavbarComponent, LoginComponent],
+  imports: [CommonModule, RouterOutlet, NgOptimizedImage, NavbarComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-
   childEmition: string = "";
+
+  logoSaishoUrl: string = "/assets/images/logoSaisho.png"
 
   receiveEmition($event: string): void {
     this.childEmition = $event;
