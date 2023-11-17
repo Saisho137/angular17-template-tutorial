f17a3175244a9e564fe4a6d007006a8d1b6549da
Santiago <santib.2001@hotmail.com>
2023-11-17 16:12:36 -0500

    feat: learning about static and reactive forms on Angular

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,13 +1,37 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {FormsModule, ReactiveFormsModule, FormControl, FormGroup} from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  email: String = "";
+  password: String = "";
+  apiResponse: any = "";
+
+  profileForm = new FormGroup({
+    name: new FormControl(''),
+    mail: new FormControl(''),
+  });
+
+  handleReactiveSubmit(): void {
+    alert(
+      this.profileForm.value.name + ' | ' + this.profileForm.value.mail
+    );
+  }
+
+  public validateUser(): void {
+    if (!this.email || !this.password) {
+      window.alert("You should fill all the fields.")
+      return
+    }
+  }
 
 }
